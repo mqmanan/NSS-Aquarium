7ddf810030906538f330f017b46c7450fc4296cb
Maria Manansala <mqmanansala@gmail.com>
2022-09-06 18:23:26 -0400

    first fish draft

---
+++ scripts/main.js
@@ -0,0 +1,90 @@
+const martinsFish = [
+    { id: 1, 
+      name: "Rick Sanchez", 
+      species: "Sardines", 
+      length: "20 inches", 
+      location: "Unity's Planet", 
+      diet: "Plankton" 
+    },
+    { id: 2, 
+      name: "Morty Smith", 
+      species: "Anchovies", 
+      length: "7 inches", 
+      location: "Pawn Shop Planet", 
+      diet: "Plankton" 
+    },
+    { id: 3, 
+      name: "Summer Smith", 
+      species: "Tilapia", 
+      length: "10 inches", 
+      location: "Gazorpazorp", 
+      diet: "Worms" 
+    },
+    { id: 4, 
+      name: "Beth Smith", 
+      species: "Salmon", 
+      length: "15 inches", 
+      location: "Earth", 
+      diet: "Shrimps" 
+    },
+    { id: 5, 
+      name: "Jerry Smith", 
+      species: "Guppy", 
+      length: "1 inch", 
+      location: "Pluto", 
+      diet: "Algae" 
+    }
+    ]
+
+// const careTips = [
+//     { id: 1, 
+//       name: "Rick Sanchez", 
+//       species: "Sardines", 
+//       care: "Tank Water Temp = 75°, Tank Water Salinity = 1.024" 
+//     },
+//     { id: 2, 
+//       name: "Morty Smith", 
+//       species: "Anchovies", 
+//       care: "Tank Water Temp = 78°, Tank Water Salinity = 1.024"
+//     },
+//     { id: 3, 
+//       name: "Summer Smith", 
+//       species: "Tilapia", 
+//       care: "Tank Water Temp = 78°, Tank Water Salinity = 1.024"
+//     },
+//     { id: 4, 
+//       name: "Beth Smith", 
+//       species: "Salmon", 
+//       care: "Tank Water Temp = 75°, Tank Water Salinity = 1.024"
+//     },
+//     { id: 5, 
+//       name: "Jerry Smith", 
+//       species: "Guppy", 
+//       care: "Tank Water Temp = 80°, Tank Water Salinity = 1.024"
+//     },
+//     ]
+
+// const visitedPlaces = [ "Unity's Planet", "Pawn Shop Planet", "Gazorpazorp", "Earth", "Pluto" ]
+
+// const characterQuotes = [
+//     { id: 1, 
+//       name: "Rick Sanchez",
+//       quote: "To live is to risk it all."
+//     },
+//     { id: 2, 
+//       name: "Morty Smith", 
+//       quote: "Sometimes science is more art than science." 
+//     },
+//     { id: 3, 
+//       name: "Summer Smith", 
+//       quote: "Camping is just being homeless without the change." 
+//     },
+//     { id: 4, 
+//       name: "Beth Smith", 
+//       quote: "You can't make people like you. You just have to wait for hating you to bore them." 
+//     },
+//     { id: 5, 
+//       name: "Jerry Smith", 
+//       quote: "Life is effort, and I'll stop when I die!" 
+//     }
+//     ]
